2db646c719ed5d35bafa0f23a134c4f7c66bf976
harshvaghanii <harshvaghani98@gmail.com>
2024-10-07 20:57:56 -0400

    adding dto code

--- backend/src/main/java/com/bu/harshvaghani/backend/dto/UserDTO.java
+++ backend/src/main/java/com/bu/harshvaghani/backend/dto/UserDTO.java
@@ -1,4 +1,18 @@
 package com.bu.harshvaghani.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
+
